8084df2497290edc9b26fcf0449e5c3ea7900ee3
Orlando Caraballo <orlandocaraballo@gmail.com>
2021-11-20 23:57:18 -0500

    heroku: got heroku working, turns out it wasn't in the code :(... reverting all test changes

--- server/routes/students.js
+++ server/routes/students.js
@@ -1,43 +1,14 @@
 import express from "express";
-// import { studentsCollection, client } from "../db.js";
-import { ObjectId, MongoClient } from "mongodb";
+import { studentsCollection } from "../db.js";
+import { ObjectId } from "mongodb";
 
 const studentsRouter = express.Router();
 
 studentsRouter.get("/", async (req, res, next) => {
+  const studentsCursor = studentsCollection.find();
+
   try {
-    const {
-      NODE_ENV,
-      DB_CONNECTION_STRING: ENV_DB_CONNECTION_STRING,
-      PORT,
-    } = process.env;
-
-    const connectionString =
-      NODE_ENV === "production"
-        ? ENV_DB_CONNECTION_STRING
-        : "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
-
-    console.log({ NODE_ENV, connectionString, PORT });
-
-    const client = new MongoClient(connectionString, {
-      monitorCommands: true,
-    });
-
-    // setup our monitoring
-    client.on("commandStarted", console.debug);
-    client.on("commandSucceeded", console.debug);
-    client.on("commandFailed", console.debug);
-
-    await client.connect();
-
-    const db = client.db("software-engineering-immersive");
-
-    const studentsCollection = db.collection("students");
-
-    const studentsCursor = studentsCollection.find();
     const students = await studentsCursor.toArray();
-
-    await client.close();
 
     res.json(students);
   } catch (err) {
@@ -45,77 +16,77 @@ studentsRouter.get("/", async (req, res, next) => {
   }
 });
 
-// studentsRouter.get("/:id", async ({ params: { id } }, res, next) => {
-//   try {
-//     const student = await studentsCollection.findOne({
-//       _id: new ObjectId(id),
-//     });
+studentsRouter.get("/:id", async ({ params: { id } }, res, next) => {
+  try {
+    const student = await studentsCollection.findOne({
+      _id: new ObjectId(id),
+    });
 
-//     if (!student) {
-//       return res
-//         .status(404)
-//         .json({ message: `Student with id ${id} not found` });
-//     }
+    if (!student) {
+      return res
+        .status(404)
+        .json({ message: `Student with id ${id} not found` });
+    }
 
-//     res.json(student);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+    res.json(student);
+  } catch (err) {
+    next(err);
+  }
+});
 
-// studentsRouter.post("/", async ({ body }, res, next) => {
-//   try {
-//     const { name, gender, knownFor, github, slackHandle } = body;
+studentsRouter.post("/", async ({ body }, res, next) => {
+  try {
+    const { name, gender, knownFor, github, slackHandle } = body;
 
-//     await studentsCollection.insertOne({
-//       name,
-//       gender,
-//       knownFor,
-//       github,
-//       slackHandle,
-//     });
+    await studentsCollection.insertOne({
+      name,
+      gender,
+      knownFor,
+      github,
+      slackHandle,
+    });
 
-//     res.status(201).json({ message: "Student was created" });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+    res.status(201).json({ message: "Student was created" });
+  } catch (err) {
+    next(err);
+  }
+});
 
-// studentsRouter.put("/:id", async ({ params: { id }, body }, res, next) => {
-//   try {
-//     const { name, gender, knownFor, github, slackHandle } = body;
+studentsRouter.put("/:id", async ({ params: { id }, body }, res, next) => {
+  try {
+    const { name, gender, knownFor, github, slackHandle } = body;
 
-//     const dbResponse = await studentsCollection.updateOne(
-//       { _id: new ObjectId(id) },
-//       { $set: { name, gender, knownFor, github, slackHandle } }
-//     );
+    const dbResponse = await studentsCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { name, gender, knownFor, github, slackHandle } }
+    );
 
-//     // im assuming this means the student was not in the system
-//     if (dbResponse.modifiedCount === 0) {
-//       return res.status(404).json({ message: "Student was not updated" });
-//     }
+    // im assuming this means the student was not in the system
+    if (dbResponse.modifiedCount === 0) {
+      return res.status(404).json({ message: "Student was not updated" });
+    }
 
-//     res.json({ message: "Student has been updated" });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+    res.json({ message: "Student has been updated" });
+  } catch (err) {
+    next(err);
+  }
+});
 
-// studentsRouter.delete("/:id", async ({ params: { id } }, res, next) => {
-//   try {
-//     const dbResponse = await studentsCollection.deleteOne({
-//       _id: new ObjectId(id),
-//     });
+studentsRouter.delete("/:id", async ({ params: { id } }, res, next) => {
+  try {
+    const dbResponse = await studentsCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
 
-//     // im assuming this means the student was not in the system
-//     if (dbResponse.deletedCount === 0) {
-//       return res.status(404).json({ message: "Student was not deleted" });
-//     }
+    // im assuming this means the student was not in the system
+    if (dbResponse.deletedCount === 0) {
+      return res.status(404).json({ message: "Student was not deleted" });
+    }
 
-//     res.json({ message: "Student has been deleted" });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+    res.json({ message: "Student has been deleted" });
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default studentsRouter;
